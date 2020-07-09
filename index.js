080df4a97d7ef1b7b52d21b611e029636807f94a
Kirill Krupina <kirill.krupina26@gmail.com>
2020-07-09 20:03:03 +0300

    Adding express-handlebars

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+});
+
+// registration
+app.engine('hbs', hbs.engine);
+
+// setting value(hbs) to name(view engine) 
+app.set('view engine', 'hbs');
+app.set('views', 'views')
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
